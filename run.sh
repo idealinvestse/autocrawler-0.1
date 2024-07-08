36f81c008bad14e37dce643840cd04abf1475f86
idealinvestse <oscar@idealinvest.se>
2024-07-08 13:09:03 +0200

    modified:   .vscode/settings.json 	new file:   __pycache__/crawler_1.cpython-311.pyc 	modified:   __pycache__/utils.cpython-311.pyc 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 pip install -r requirements.txt
 
 # Run the FastAPI application
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Run the frontend
 npm start
